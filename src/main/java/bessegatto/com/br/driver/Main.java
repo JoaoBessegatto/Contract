0f058f008aef479f7fe3c02c3a466a8fa5b1449b
João Pedro Bessegatto <bfjoaop@gmail.com>
2025-04-03 21:41:08 -0300

    Entidades criadas.

--- src/main/java/bessegatto/com/br/driver/Main.java
+++ src/main/java/bessegatto/com/br/driver/Main.java
@@ -1,7 +1,27 @@
 package bessegatto.com.br.driver;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato: ");
+        System.out.print("Numero do contrato: ");
+        int number = sc.nextInt();
+        System.out.print("Data: ");
+        LocalDate data = LocalDate.parse(sc.nextLine(),sdf);
+        System.out.print("Valor do contrato: ");
+        double valor = sc.nextDouble();
+        System.out.print("Quantidade de parcelas: ");
+        int parcelas = sc.nextInt();
+
     }
 }
